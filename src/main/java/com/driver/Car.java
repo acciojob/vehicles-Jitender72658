dda97d9710aaf81c907d5a5f3437ec535ab23828
Jitender Kumar <jitenderkumar72658@gmail.com>
2023-02-13 02:17:50 +0530

    Constructor of Boat class created.

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,3 +1,93 @@
+//package com.driver;
+//
+//public class Car extends Vehicle {
+//    private int wheels;
+//    private String type;
+//    private int doors;
+//    private int gears;
+//    private boolean isManual;
+//    private int currentGear;
+//    private int seats;
+//
+//    public int getWheels() {
+//        return wheels;
+//    }
+//
+//    public void setWheels(int wheels) {
+//        this.wheels = wheels;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+//
+//    public void setType(String type) {
+//        this.type = type;
+//    }
+//
+//    public int getDoors() {
+//        return doors;
+//    }
+//
+//    public void setDoors(int doors) {
+//        this.doors = doors;
+//    }
+//
+//    public int getGears() {
+//        return gears;
+//    }
+//
+//    public void setGears(int gears) {
+//        this.gears = gears;
+//    }
+//
+//    public int getCurrentGear() {
+//        return currentGear;
+//    }
+//
+//    public void setCurrentGear(int currentGear) {
+//        this.currentGear = currentGear;
+//    }
+//
+//    public int getSeats() {
+//        return seats;
+//    }
+//
+//    public void setSeats(int seats) {
+//        this.seats = seats;
+//    }
+//
+//    public boolean isManual() {
+//        return isManual;
+//    }
+//
+//    public void setManual(boolean manual) {
+//        isManual = manual;
+//    }
+//
+//    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+//        //Hint: Car extends Vehicle
+//        super(name);
+//        this.wheels = wheels;
+//        this.doors = doors;
+//        this.gears = gears;
+//        this.isManual = isManual;
+//        this.type = type;
+//        this.seats = seats;
+//        this.currentGear =1;
+//    }
+//
+//    public void changeGear(int newGear){
+//             this.currentGear= newGear;
+//        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+//    }
+//
+//    public void changeSpeed(int newSpeed, int newDirection){
+//           super.move(newSpeed,newDirection);
+//        System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+//    }
+//}
+
 package com.driver;
 
 public class Car extends Vehicle {
@@ -41,6 +131,14 @@ public class Car extends Vehicle {
         this.gears = gears;
     }
 
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public void setManual(boolean manual) {
+        isManual = manual;
+    }
+
     public int getCurrentGear() {
         return currentGear;
     }
@@ -57,33 +155,25 @@ public class Car extends Vehicle {
         this.seats = seats;
     }
 
-    public boolean isManual() {
-        return isManual;
-    }
-
-    public void setManual(boolean manual) {
-        isManual = manual;
-    }
-
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
         super(name);
+        this.currentGear = 1;
         this.wheels = wheels;
         this.doors = doors;
         this.gears = gears;
         this.isManual = isManual;
         this.type = type;
         this.seats = seats;
-        this.currentGear =1;
     }
 
     public void changeGear(int newGear){
-             this.currentGear= newGear;
+        this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-           super.move(newSpeed,newDirection);
+        super.move(newSpeed,newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
